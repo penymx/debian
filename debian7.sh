1bfa1f5a55032d809c5b8732a98cfd6d36b574b2
najahiiii <31695903+najahiiii@users.noreply.github.com>
2018-03-31 14:26:32 +0700

    update badvpn listen port to 7200

--- debian7.sh
+++ debian7.sh
@@ -153,10 +153,10 @@ iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
 iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
 
 # install badvpn
-wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/najahiiii/debian/master/badvpn-udpgw"
-sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/najahiiii/debian/master/badvpn-udpgw64"
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200' /etc/rc.local
 chmod +x /usr/bin/badvpn-udpgw
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200
 
 # install mrtg
 apt-get -y install snmpd;
